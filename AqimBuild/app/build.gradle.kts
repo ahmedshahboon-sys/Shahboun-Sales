26aa5c72846fd3e5ff1973cc396263438d1943de
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-16 19:06:24 +0200

    Configure Aqim unsigned release build

---
+++ AqimBuild/app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "com.shahboun.aqim"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.shahboun.aqim"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+    buildTypes { release { isMinifyEnabled = false } }
+}
