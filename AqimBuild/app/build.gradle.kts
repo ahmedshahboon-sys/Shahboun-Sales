36a7e2bd6422a66594f6169097c99616a270bda3
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-16 20:46:26 +0200

    Bump AQIM to 1.4.0

--- AqimBuild/app/build.gradle.kts
+++ AqimBuild/app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.shahboun.aqim"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
     }
     buildTypes { release { isMinifyEnabled = false } }
 }
