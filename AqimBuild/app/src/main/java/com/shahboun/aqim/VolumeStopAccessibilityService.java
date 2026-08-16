68b721d0b6cbcf2b6d5f5d760231613bcca3c40f
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-16 22:04:52 +0200

    AQIM 1.5 compile shim without accessibility service

---
+++ AqimBuild/app/src/main/java/com/shahboun/aqim/VolumeStopAccessibilityService.java
@@ -0,0 +1,3 @@
+package com.shahboun.aqim;
+/** Compatibility marker only. Not registered as an Android AccessibilityService. */
+public final class VolumeStopAccessibilityService { private VolumeStopAccessibilityService() {} }
