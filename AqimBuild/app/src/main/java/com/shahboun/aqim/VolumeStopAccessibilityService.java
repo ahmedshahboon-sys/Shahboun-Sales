e27b07f18eb1b6b9617c751da16b7fca42b8b9c9
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-17 13:12:36 +0200

    refactor(aqim): remove obsolete accessibility compatibility marker

--- AqimBuild/app/src/main/java/com/shahboun/aqim/VolumeStopAccessibilityService.java
+++
@@ -1,3 +0,0 @@
-package com.shahboun.aqim;
-/** Compatibility marker only. Not registered as an Android AccessibilityService. */
-public final class VolumeStopAccessibilityService { private VolumeStopAccessibilityService() {} }
